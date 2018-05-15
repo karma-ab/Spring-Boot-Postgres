6b80b310c188d011c60ddfaa55d58946bb5b55dd
Abhijit <vishwakarma.abhijit@gmail.com>
2018-05-16 00:48:33 +0530

    implementedCORS filter and AOP logger contains bug

--- src/main/java/com/project/demo/service/CityService.java
+++ src/main/java/com/project/demo/service/CityService.java
@@ -39,14 +39,12 @@ public class CityService implements ICityService {
 	@Override
 	public List<CityDTO> findAll() {
 
-		List<CityMaster> cities = (List<CityMaster>) cityRepository.findAll();
-		List<CityDTO> cityDto = new ArrayList<CityDTO>();
-		for (CityMaster city : cities) {
-			CityDTO obj = new CityDTO(city.getId(), city.getName(), city.getPopulation());
-			cityDto.add(obj);
-		}
+		List<CityMaster> cities = (List<CityMaster>) cityRepository.findAll();		
+		ModelMapper mapper = new ModelMapper();
+		Type listType = new TypeToken<List<CityDTO>>() {
+		}.getType();
 		log.info("City returned to the user");
-		return cityDto;
+		return mapper.map(cities, listType);
 	}
 
 	@Override
@@ -59,24 +57,12 @@ public class CityService implements ICityService {
 		cities = mapper.map(cityList, listType);
 		cityRepository.saveAll(cities);
 		log.info("Cities added to the database");
-		/*
-		 * cityList.forEach(city->{ CityMaster cityPersist = new CityMaster();
-		 * cityPersist.setName(city.getName());
-		 * cityPersist.setPopulation(city.getPopulation());
-		 * cities.add(cityRepository.save(cityPersist)); });
-		 */ return cities;
+		return cities;
 	}
 
 	@Override
 	public List<MayorMaster> addMayor(List<MayorDTO> mayorList) {
 		List<MayorMaster> mayors = new ArrayList<>();
-
-		/*
-		 * ModelMapper mapper = new ModelMapper(); Type listType = new
-		 * TypeToken<List<MayorMaster>>() {}.getType(); mayors =
-		 * mapper.map(mayorList,listType); mayorRepository.saveAll(mayors);
-		 */
-
 		mayorList.forEach(mayor -> {
 			MayorMaster mayorPersist = new MayorMaster();
 			mayorPersist.setName(mayor.getName());
@@ -91,20 +77,18 @@ public class CityService implements ICityService {
 	public List<MayorDTO> findMayors() {
 		List<MayorMaster> mayors = (List<MayorMaster>) mayorRepository.findAll();
 		ModelMapper mapper = new ModelMapper();
-		Type listType = new TypeToken<List<MayorDTO>>() {
-		}.getType();
+		Type listType = new TypeToken<List<MayorDTO>>() {}.getType();
+		log.info("Mayors List returned");
 		return mapper.map(mayors, listType);
 	}
 
 	@Override
 	public MessageDTO addCityMayor(List<CityMayorDTO> obj) {
 
-		obj.forEach(cityMayor -> {
+		obj.forEach(cityMayor -> 
 			cityMayorRepository.save(new CityMayor(cityRepository.findById(cityMayor.getCityId()).get(),
-					mayorRepository.findById(cityMayor.getMayorId()).get()));
-		});
-
-		MessageDTO msg = new MessageDTO("insertSuccessful");
-		return msg;
+					mayorRepository.findById(cityMayor.getMayorId()).get()))
+		);
+		return new MessageDTO("insertSuccessful");
 	}
 }
